105749a2d619aa68d3bcc6990bf4845cc5f2ad86
KillSwitch999 <140569515+KillSwitch999@users.noreply.github.com>
2025-05-29 18:00:30 -0400

    Weekly Challenges Set 1

--- ChallengesWithTestsMark8/ChallengesSet01.cs
+++ ChallengesWithTestsMark8/ChallengesSet01.cs
@@ -1,49 +1,59 @@
 ﻿using System;
+using System.Diagnostics;
+using Xunit;
 
 namespace ChallengesWithTestsMark8
 {
     public class ChallengesSet01
     {
-        public bool AreTwoNumbersTheSame(int num1, int num2)
+        public bool AreTwoNumbersTheSame(int number1, int number2)
         {
-            if(num1 == num2)
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
+            return number1 == number2;
         }
 
         public double Subtract(double minuend, double subtrahend)
         {
-            throw new NotImplementedException();
+            return minuend - subtrahend;
+
         }
 
         public int Add(int number1, int number2)
-        {
-            throw new NotImplementedException();
-        }
+            {
+                return number1 + number2;
+            }
 
-        public int GetSmallestNumber(int number1, int number2)
-        {
-            throw new NotImplementedException();
-        }
+            public int GetSmallestNumber(int number1, int number2)
+            {
+                return number1 < number2 ? number1 : number2;
+                
+                //if (number1 < number2)
+                //{
+                //    return number1;
+                //}
+                //return number 2;
+            }
 
         public long Multiply(long factor1, long factor2)
         {
-            throw new NotImplementedException();
+            return factor1 * factor2;
         }
 
         public string GetGreeting(string nameOfPerson)
         {
-            throw new NotImplementedException();
+            //if (nameOfPerson == "")
+            //{
+            //    return "Hello";
+            //}
+            //else
+            //   return $"Hello, {nameOfPerson}";
+            //}
+
+            return string.IsNullOrEmpty(nameOfPerson) ? "Hello!" : $"Hello, {nameOfPerson}!";
         }
 
-        public string GetHey()
-        {
-            throw new NotImplementedException();
-        }
+        public string GetHey() => "HEY!";
+        //{
+        //    return "HEY!"
+        //}
     }
 }
